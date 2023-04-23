e208161d283b30337a37f01613b9d4ea4a963d9c
Natalija <milashk222@gmail.com>
2023-04-23 22:04:36 +0300

    project post

---
+++ src/test/java/pageobject/TicketsTestOnPages.java
@@ -0,0 +1,56 @@
+package pageobject;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pageobject.model.Passenger;
+import pageobject.pages.HomePage;
+import pageobject.pages.PassengerInfoPage;
+import pageobject.pages.SeatSelectionPage;
+import pageobject.pages.SuccessfulRegistrationPage;
+
+public class TicketsTestOnPages {
+    private final String URL = "http://www.qaguru.lv:8089/tickets/";
+    private final String FROM_AIRPORT = "RIX";
+    private final String TO_AIRPORT = "SFO";
+    private BaseFunc baseFunc = new BaseFunc();
+
+    @Test
+    public void successfulRegistrationTest() {
+        Passenger passenger = new Passenger("TestName", "TestSurname", "CCCVVV",
+                2, 1, 4, "16-05-2018");
+
+        int seatNr = RandomUtils.nextInt(1, 35);
+
+        baseFunc.openUrl(URL);
+        HomePage homePage = new HomePage(baseFunc);
+        homePage.selectAirports(FROM_AIRPORT, TO_AIRPORT);
+
+        PassengerInfoPage infoPage = new PassengerInfoPage(baseFunc);
+        infoPage.fillInPassengerInfo(passenger);
+
+        Assertions.assertEquals(passenger.getFirstName(), infoPage.getPassengerName(), "Wrong name!");
+        Assertions.assertEquals(FROM_AIRPORT, infoPage.getFirstFromAirport(), "Error msg");
+        Assertions.assertEquals(FROM_AIRPORT, infoPage.getSecondFromAirport(), "Error msg");
+        Assertions.assertEquals(TO_AIRPORT, infoPage.getFirstToAirport(), "Error msg");
+        Assertions.assertEquals(TO_AIRPORT, infoPage.getSecondToAirport(), "Error msg");
+
+        Assertions.assertTrue(infoPage.getPrice().length() > 0, "Error msg");
+
+        infoPage.book();
+
+        SeatSelectionPage seatSelectionPage = new SeatSelectionPage(baseFunc);
+        seatSelectionPage.selectSeat(seatNr);
+        int selectedSeat = seatSelectionPage.getSelectedSeatNr();
+        Assertions.assertEquals(seatNr, selectedSeat, "Wrong seat selected");
+
+        seatSelectionPage.book();
+
+        SuccessfulRegistrationPage successfulRegistrationPage = new SuccessfulRegistrationPage(baseFunc);
+        Assertions.assertTrue(successfulRegistrationPage.isSuccessfulRegistrationTextAppears(), "Wrong text");
+
+        baseFunc.closeBrowser();
+    }
+}
